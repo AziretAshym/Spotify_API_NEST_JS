d32bbe238db1de98c402e49814ece68f90c7354e
Ashymov Aziret <aziretashym@gmail.com>
2025-02-12 22:12:02 +0600

    Created a schema for artists, and made methods for them (get. post, delete)

---
+++ src/artists/artists.controller.ts
@@ -0,0 +1,68 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    NotFoundException,
+    Param,
+    Post,
+    UploadedFile,
+    UseInterceptors
+} from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Artist, ArtistDocument} from "../schemas/artist.schema";
+import {Model} from "mongoose";
+import {CreateArtistDto} from "./create-artist.dto";
+import {FileInterceptor} from "@nestjs/platform-express";
+import * as path from "node:path";
+
+@Controller('artists')
+export class ArtistsController {
+    constructor(
+        @InjectModel(Artist.name)
+        private artistModel: Model<ArtistDocument>
+    ) {}
+
+    @Get()
+    async getArtists() {
+        return this.artistModel.find();
+    };
+
+    @Get(':id')
+    getOneArtist(@Param('id') id: string) {
+        return this.artistModel.findById(id);
+    }
+    @Post()
+    @UseInterceptors(
+        FileInterceptor('image', { dest: './public/uploads/artists' }),
+    )
+    async createArtist(
+        @UploadedFile() file: Express.Multer.File,
+        @Body() artistDto: CreateArtistDto
+    ) {
+
+
+        const artist = new this.artistModel({
+            name: artistDto.name,
+            info: artistDto.info,
+            image: file && file.filename ? 'uploads/artists/' + file.filename : null,
+            isPublished: artistDto.isPublished,
+        });
+
+        const newArtist = await artist.save();
+        return newArtist;
+    }
+
+
+
+
+
+    @Delete(':_id')
+    async deleteArtist(@Param('_id') _id: string) {
+        const artist = await this.artistModel.findByIdAndDelete(_id);
+        if (!artist) {
+            throw new NotFoundException("Artist not found!");
+        }
+        return { message: 'Artist deleted successfully!' };
+    }
+}
